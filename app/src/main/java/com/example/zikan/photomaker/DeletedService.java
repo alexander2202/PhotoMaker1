6638cb553cde3b7bd12c43793abf6050cbb81aac
alexander2202 <bigzikan@mail.ru>
2016-04-06 11:26:33 +0500

    All files

--- app/src/main/java/com/example/zikan/photomaker/DeletedService.java
+++ app/src/main/java/com/example/zikan/photomaker/DeletedService.java
@@ -3,24 +3,14 @@ package com.example.zikan.photomaker;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.widget.ListView;
 
 import java.util.ArrayList;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 
 public class DeletedService extends Service {
 
-    ListView NewPhotos = (ListView)findViewById(R.id.NewPhotos);
+    /*ListView NewPhotos = (ListView)findViewById(R.id.NewPhotos);
     ArrayList PhNames = new ArrayList();
-    ListView DeletedPhotos = (ListView)findViewById(R.id.DeletedPhotos);
+    ListView DeletedPhotos = (ListView)findViewById(R.id.DeletedPhotos);*/
     ArrayList DelNames = new ArrayList();
     public final static String EXTRA_MESSAGE ="com.exmple.zikan.photomaker.MESSAGE";
 
